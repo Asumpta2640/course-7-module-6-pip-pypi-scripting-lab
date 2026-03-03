82cf1ec785bc1d75469c4918c508b0cbf4cd8e0d
Jelagat2640 <sugutasumpta@gmail.com>
2026-03-03 04:14:14 +0300

    generate_log.py

--- lib/generate_log.py
+++ lib/generate_log.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import os
+#import os
 
 def generate_log(data):
     # TODO: Implement log generation logic
@@ -19,7 +19,7 @@ def generate_log(data):
 
 
     # STEP 4: Print a confirmation message with the filename
-    print(f"Log file '{filename}' has been created successfully.")
+    #print(f"Log file '{filename}' has been created successfully.")
 
     return filename  
     
